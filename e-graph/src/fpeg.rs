9db58d29b793cacfab62f8d768ef551e13637b25
jordanaq <quinn.jordan@protonmail.ch>
2025-07-02 02:18:37 -0400

    Changed prim to lit and added prim op node

--- e-graph/src/fpeg.rs
+++ e-graph/src/fpeg.rs
@@ -1,41 +1,52 @@
 use std::fmt;
 
-use egg::*;
+use egg::{
+    define_language,
+    Id
+};
 
 use crate::parse::*;
 
+
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub enum Prim {
+pub enum Lit {
     Word8(u8),
     Word32(u32),
+    Word64(u64),
 }
 
-impl fmt::Display for Prim {
+impl fmt::Display for Lit {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            Prim::Word8(v)   => write!(f, "Word8({})", v),
-            Prim::Word32(v)  => write!(f, "Word32({})", v),
+            Lit::Word8(v)   => write!(f, "Word8({})", v),
+            Lit::Word32(v)  => write!(f, "Word32({})", v),
+            Lit::Word64(v)  => write!(f, "Word64({})", v),
         }
     }
 }
 
 #[derive(Debug, PartialEq, Eq)]
-pub struct ParsePrimErr;
+pub struct ParseLitErr;
 
-impl std::str::FromStr for Prim {
-    type Err = ParsePrimErr;
+impl std::str::FromStr for Lit {
+    type Err = ParseLitErr;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match parse_prim(s) {
-            Ok(("", prim)) => Ok(prim),
-            Ok(_) => Err(ParsePrimErr),
-            Err(_) => Err(ParsePrimErr),
+        match parse_lit(s) {
+            Ok(("", lit)) => Ok(lit),
+            Ok(_) => Err(ParseLitErr),
+            Err(_) => Err(ParseLitErr),
         }
     }
 }
 
+type Type = String;
+pub struct Prim(String, Vec<Type>);
+
 define_language! {
     pub enum FPeg {
-        Prim(Prim),
+        Lit(Lit),
+        Arg(String),
+        "Prim" = Prim(Box<[Id]>),
     }
 }
--- e-graph/src/parse.rs
+++ e-graph/src/parse.rs
@@ -9,10 +9,6 @@ use nom::{
         tag,
         take_while1,
     },
-    number::complete::{
-        u8 as parse_u8,
-        u32 as parse_u32,
-    },
     sequence::{
         preceded,
         terminated,
@@ -22,8 +18,7 @@ use nom::{
 };
 
 use crate::fpeg::{
-    FPeg,
-    Prim,
+    Lit,
 };
 
 
@@ -32,18 +27,23 @@ fn parse_hex(s: &str) -> IResult<&str, u64> {
     Ok((rest, u64::from_str_radix(hex, 16).unwrap()))
 }
 
-fn parse_w8(s: &str) -> IResult<&str, Prim> {
+fn parse_w8(s: &str) -> IResult<&str, Lit> {
     let (rest, w64): (&str, u64) = terminated(parse_hex, tag(":w8")).parse(s)?;
-    Ok((rest, Prim::Word8(u8::try_from(w64).unwrap())))
+    Ok((rest, Lit::Word8(u8::try_from(w64).unwrap())))
 }
 
-fn parse_w32(s: &str) -> IResult<&str, Prim> {
+fn parse_w32(s: &str) -> IResult<&str, Lit> {
     let (rest, w64) = terminated(parse_hex, tag(":w32")).parse(s)?;
-    Ok((rest, Prim::Word32(u32::try_from(w64).unwrap())))
+    Ok((rest, Lit::Word32(u32::try_from(w64).unwrap())))
 }
 
-pub fn parse_prim(s: &str) -> IResult<&str, Prim> {
-    alt((parse_w8, parse_w32)).parse(s)
+fn parse_w64(s: &str) -> IResult<&str, Lit> {
+    let (rest, w64) = terminated(parse_hex, tag(":w64")).parse(s)?;
+    Ok((rest, Lit::Word64(w64)))
+}
+
+pub fn parse_lit(s: &str) -> IResult<&str, Lit> {
+    alt((parse_w8, parse_w32, parse_w64)).parse(s)
 }
 
 
@@ -58,20 +58,27 @@ mod tests {
 
     #[test]
     fn test_parse_w8() {
-        assert_eq!(parse_w8("0x0:w8"), Ok(("", Prim::Word8(0))));
-        assert_eq!(parse_w8("0xB:w8"), Ok(("", Prim::Word8(11))));
-        assert_eq!(parse_w8("0xb:w8"), Ok(("", Prim::Word8(0xb))));
+        assert_eq!(parse_w8("0x0:w8"), Ok(("", Lit::Word8(0))));
+        assert_eq!(parse_w8("0xB:w8"), Ok(("", Lit::Word8(11))));
+        assert_eq!(parse_w8("0xb:w8"), Ok(("", Lit::Word8(0xb))));
     }
 
     #[test]
     fn test_parse_w32() {
-        assert_eq!(parse_w32("0x0:w32"), Ok(("", Prim::Word32(0))));
-        assert_eq!(parse_w32("0x10000:w32"), Ok(("", Prim::Word32(0x10000))));
+        assert_eq!(parse_w32("0x0:w32"), Ok(("", Lit::Word32(0))));
+        assert_eq!(parse_w32("0x10000:w32"), Ok(("", Lit::Word32(0x10000))));
     }
 
     #[test]
-    fn parse_prims() {
-        assert_eq!(parse_prim("0x0:w8"), Ok(("", Prim::Word8(0))));
-        assert_eq!(parse_prim("0x0:w32"), Ok(("", Prim::Word32(0))));
+    fn test_parse_w64() {
+        assert_eq!(parse_w64("0x0:w64"), Ok(("", Lit::Word64(0))));
+        assert_eq!(parse_w64("0x100000000:w64"), Ok(("", Lit::Word64(0x100000000))));
+    }
+
+    #[test]
+    fn parse_lits() {
+        assert_eq!(parse_lit("0x0:w8"), Ok(("", Lit::Word8(0))));
+        assert_eq!(parse_lit("0x0:w32"), Ok(("", Lit::Word32(0))));
+        assert_eq!(parse_w64("0x0:w64"), Ok(("", Lit::Word64(0))));
     }
 }
