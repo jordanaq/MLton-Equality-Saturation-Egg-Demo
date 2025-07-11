1d87c4de17ed93e8dd2f3f11b402756ed2ba7329
jordanaq <quinn.jordan@protonmail.ch>
2025-07-10 22:59:18 -0400

    Started skeleton

--- skeleton/src/skeleton.rs
+++ skeleton/src/skeleton.rs
@@ -1,4 +1,33 @@
+use std::collections::HashMap;
+
 use e_graph::*;
 
+use sml_utils::SmlType;
 
 
+type IdType = u64;
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+struct BlockArg {
+    id: IdType,
+    arg_t: SmlType,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+enum Transfer {
+
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+struct Block {
+    id: IdType,
+    inputs: Vec<BlockArg>,
+    transfer: Transfer
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub struct Skeleton {
+    graph: FPeg,
+    root: Block,
+    blocks: HashMap<IdType, Block>,
+}
