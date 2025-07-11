7d3850765143842ba0e2f7085e5b581e5c439c0f
jordanaq <quinn.jordan@protonmail.ch>
2025-07-10 22:20:27 -0400

    Fixed word and added prim and constr

--- e-graph/src/parse.rs
+++ e-graph/src/parse.rs
@@ -1,33 +1,27 @@
 use u64;
 
 use nom::{
-    AsChar,
-    branch::{
-        alt,
-    },
-    bytes::complete::{
+    branch::alt, bytes::complete::{
         tag,
         take_while1,
-    },
-    character::multispace0,
+    }, character::multispace0,
+    combinator::recognize,
+    error::ParseError,
     multi::{
-        many0,
+        many1,
         separated_list0,
-    },
-    sequence::{
+    }, sequence::{
         pair,
         preceded,
         terminated,
-    },
-    IResult,
-    Parser,
+    }, AsChar, IResult, Parser
 };
 
-use crate::fpeg::{
+use crate::{fpeg::{
     Lit,
     Prim,
     Type,
-};
+}, Constr};
 
 
 fn surrounded<'a, O1, O2, O3, E>(
@@ -41,8 +35,11 @@ where
     preceded(before, terminated(inner, after))
 }
 
-fn word() -> impl Parser {
-    take_while1(alt((AsChar::is_alphanum, |c: char| "-_".contains(c))))
+fn word<'a, E>() -> impl Parser<&'a str, Output=&'a str, Error=E>
+where
+    E: ParseError<&'a str>
+{
+    take_while1(|c| AsChar::is_alphanum(c) || "-_".contains(c))
 }
 
 fn parse_hex(s: &str) -> IResult<&str, u64> {
@@ -69,24 +66,41 @@ pub fn parse_lit(s: &str) -> IResult<&str, Lit> {
     alt((parse_w8, parse_w32, parse_w64)).parse(s)
 }
 
-fn parse_type(s: &str) -> IResult<&str, Lit> {
-    
+fn parse_type(s: &str) -> IResult<&str, Type> {
+    let (rest, s) = word().parse(s)?;
+
+    Ok((rest, s.to_owned()))
+}
+
+fn paren_list<'a, E>() -> impl Parser<&'a str, Output=Vec<&'a str>, Error=E>
+where
+    E: ParseError<&'a str>
+{
+    surrounded(tag("("),
+               separated_list0(pair(tag(","), multispace0()), word()),
+               tag(")"))
 }
 
 pub fn parse_prim(s: &str) -> IResult<&str, Prim> {
     let (rest, (prim_s, xs)) =
-        pair(surrounded(tag("Prim<"),
-                        take_while1(AsChar::is_alpha),
-                        tag(">")),
-             surrounded(tag("("),
-                        separated_list0(pair(tag(","), multispace0), word),
-                        tag(")")))
+        (surrounded(tag("Prim<"),
+                    take_while1(AsChar::is_alpha),
+                    tag(">")),
+         paren_list()).parse(s)?;
+    let xs_p = xs.into_iter().map(String::from).collect();
                                     
-    Ok((rest, Prim(prim_s, vec![])))
+    Ok((rest, Prim(prim_s.to_string(), xs_p)))
 }
 
-pub fn parse_constr(s: &str) -> IResult<&str, Prim> {
-    todo!()
+pub fn parse_constr(s: &str) -> IResult<&str, Constr> {
+    let (rest, (constr_s, xs, type_s,)) =
+         (surrounded(tag("Constr<"), word(), tag(">")),
+          paren_list(),
+          preceded((multispace0(), tag(":"), multispace0()), parse_type)
+         ).parse(s)?;
+    let xs_p = xs.into_iter().map(String::from).collect();
+
+    Ok((rest, Constr(constr_s.to_owned(), type_s, xs_p)))
 }
 
 
@@ -119,9 +133,25 @@ mod tests {
     }
 
     #[test]
-    fn parse_lits() {
+    fn test_parse_lit() {
         assert_eq!(parse_lit("0x0:w8"), Ok(("", Lit::Word8(0))));
         assert_eq!(parse_lit("0x0:w32"), Ok(("", Lit::Word32(0))));
         assert_eq!(parse_w64("0x0:w64"), Ok(("", Lit::Word64(0))));
+    }
+
+    #[test]
+    fn test_parse_prim() {
+        let add = "Add".to_owned();
+        let xs = vec!["a".to_owned(), "b".to_owned(),];
+        assert_eq!(parse_prim("Prim<Add>(a, b)"), Ok(("", Prim(add, xs))));
+    }
+
+
+    #[test]
+    fn test_parse_constr() {
+        let add = "Add".to_owned();
+        let xs = vec!["a".to_owned(), "b".to_owned(),];
+        let t = "T".to_owned();
+        assert_eq!(parse_constr("Constr<Add>(a, b) : T"), Ok(("", Constr(add, t, xs))));
     }
 }
