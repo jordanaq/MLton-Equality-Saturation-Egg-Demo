f9cf34eda89cbb1db0098eb5d5fa08bb94788419
jordanaq <quinn.jordan@protonmail.ch>
2025-07-23 00:51:27 -0400

    Fixed lit parsing and added param test

--- e-graph/src/parse.rs
+++ e-graph/src/parse.rs
@@ -7,15 +7,12 @@ use nom::{
     character::multispace0,
     error::ParseError,
     multi::separated_list0,
-    sequence::{pair, preceded, terminated},
+    sequence::{pair, preceded, separated_pair, terminated},
 };
 
 use sml_utils::SmlType;
 
-use crate::{
-    Constr,
-    fpeg::{Lit, Prim},
-};
+use crate::fpeg::{Constr, Lit, Param, Prim};
 
 fn surrounded<'a, O1, O2, O3, E>(
     before: impl Parser<&'a str, Output = O1, Error = E>,
@@ -41,22 +38,52 @@ fn parse_hex(s: &str) -> IResult<&str, u64> {
 }
 
 fn parse_w8(s: &str) -> IResult<&str, Lit> {
-    let (rest, w64): (&str, u64) = terminated(parse_hex, tag(":w8")).parse(s)?;
-    Ok((rest, Lit::Word8(u8::try_from(w64).unwrap())))
+    let (rest, w8) =
+        terminated(
+            parse_hex,
+            (multispace0(), tag(":"), multispace0(), alt((tag("word8"), tag("w8"))))
+        ).parse(s)?;
+    Ok((rest, Lit::Word8(u8::try_from(w8).unwrap())))
+}
+
+fn parse_w16(s: &str) -> IResult<&str, Lit> {
+    let (rest, w16) =
+        terminated(
+            parse_hex,
+            (multispace0(), tag(":"), multispace0(), alt((tag("word16"), tag("w16"))) )
+        ).parse(s)?;
+    Ok((rest, Lit::Word16(u16::try_from(w16).unwrap())))
 }
 
 fn parse_w32(s: &str) -> IResult<&str, Lit> {
-    let (rest, w64) = terminated(parse_hex, tag(":w32")).parse(s)?;
-    Ok((rest, Lit::Word32(u32::try_from(w64).unwrap())))
+    let (rest, w32) =
+        terminated(
+            parse_hex,
+            (multispace0(), tag(":"), multispace0(), alt((tag("word32"), tag("w32"))) )
+        ).parse(s)?;
+    Ok((rest, Lit::Word32(u32::try_from(w32).unwrap())))
 }
 
 fn parse_w64(s: &str) -> IResult<&str, Lit> {
-    let (rest, w64) = terminated(parse_hex, tag(":w64")).parse(s)?;
-    Ok((rest, Lit::Word64(w64)))
+    let (rest, w64) =
+        terminated(
+            parse_hex,
+            (multispace0(), tag(":"), multispace0(), alt((tag("word64"), tag("w64"))) )
+        ).parse(s)?;
+    Ok((rest, Lit::Word64(u64::try_from(w64).unwrap())))
+}
+
+fn parse_unit(s: &str) -> IResult<&str, Lit> {
+    let (rest, _unit) = tag("()").parse(s)?;
+    Ok((rest, Lit::Unit))
 }
 
 pub fn parse_lit(s: &str) -> IResult<&str, Lit> {
-    alt((parse_w8, parse_w32, parse_w64)).parse(s)
+    surrounded(
+        (tag("Lit<"), multispace0()),
+        alt((parse_w8, parse_w16, parse_w32, parse_w64, parse_unit)),
+        (multispace0(), tag(">"))
+    ).parse(s)
 }
 
 fn parse_type(s: &str) -> IResult<&str, SmlType> {
@@ -98,6 +125,16 @@ pub fn parse_constr(s: &str) -> IResult<&str, Constr> {
     ))
 }
 
+pub fn parse_param(s: &str) -> IResult<&str, Param> {
+    let (rest, (param_r, t_s)) = surrounded(
+        tag("Param<"),
+        separated_pair(word(), (multispace0(), tag(":"), multispace0()), parse_type),
+        tag(">"),
+    )
+    .parse(s)?;
+    Ok((rest, Param(param_r.to_owned(), t_s)))
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -112,6 +149,12 @@ mod tests {
         assert_eq!(parse_w8("0x0:w8"), Ok(("", Lit::Word8(0))));
         assert_eq!(parse_w8("0xB:w8"), Ok(("", Lit::Word8(11))));
         assert_eq!(parse_w8("0xb:w8"), Ok(("", Lit::Word8(0xb))));
+    }
+
+    #[test]
+    fn test_parse_w16() {
+        assert_eq!(parse_w16("0x0:w16"), Ok(("", Lit::Word16(0))));
+        assert_eq!(parse_w16("0x100:w16"), Ok(("", Lit::Word16(0x100))));
     }
 
     #[test]
@@ -130,10 +173,17 @@ mod tests {
     }
 
     #[test]
+    fn test_parse_unit() {
+        assert_eq!(parse_unit("()"), Ok(("", Lit::Unit)));
+    }
+
+    #[test]
     fn test_parse_lit() {
-        assert_eq!(parse_lit("0x0:w8"), Ok(("", Lit::Word8(0))));
-        assert_eq!(parse_lit("0x0:w32"), Ok(("", Lit::Word32(0))));
-        assert_eq!(parse_w64("0x0:w64"), Ok(("", Lit::Word64(0))));
+        assert_eq!(parse_lit("Lit<0x0:w8>"), Ok(("", Lit::Word8(0))));
+        assert_eq!(parse_lit("Lit<0x0:w16>"), Ok(("", Lit::Word16(0))));
+        assert_eq!(parse_lit("Lit<0x0:w32>"), Ok(("", Lit::Word32(0))));
+        assert_eq!(parse_lit("Lit<0x0:w64>"), Ok(("", Lit::Word64(0))));
+        assert_eq!(parse_lit("Lit<()>"), Ok(("", Lit::Unit)));
     }
 
     #[test]
@@ -156,5 +206,12 @@ mod tests {
                 }
             ))
         );
+    }
+
+    #[test]
+    fn test_parse_param() {
+        let x = "x".to_owned();
+        let t = "T".to_owned();
+        assert_eq!(parse_param("Param<x : T>"), Ok(("", Param(x, t))));
     }
 }
