c9778f5a8901bf749f0ee1003f7f5dde5093d36d
jordanaq <quinn.jordan@protonmail.ch>
2025-07-23 00:52:50 -0400

    Fixed lit and added test for parsing FPeg lang

--- e-graph/src/fpeg.rs
+++ e-graph/src/fpeg.rs
@@ -1,6 +1,6 @@
 use std::{fmt, str::FromStr};
 
-use egg::{Id, define_language};
+use egg::{EGraph, Id, RecExpr, define_language};
 
 use sml_utils::SmlType;
 
@@ -9,6 +9,7 @@ use crate::parse::*;
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
 pub enum Lit {
     Word8(u8),
+    Word16(u16),
     Word32(u32),
     Word64(u64),
     Unit,
@@ -17,10 +18,11 @@ pub enum Lit {
 impl fmt::Display for Lit {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            Lit::Word8(v) => write!(f, "Word8({})", v),
-            Lit::Word32(v) => write!(f, "Word32({})", v),
-            Lit::Word64(v) => write!(f, "Word64({})", v),
-            Lit::Unit => write!(f, "Unit()"),
+            Lit::Word8(v) => write!(f, "Lit<{:#x} : word8>", v),
+            Lit::Word16(v) => write!(f, "Lit<{:#x} : word16>", v),
+            Lit::Word32(v) => write!(f, "Lit<{:#x} : word32>", v),
+            Lit::Word64(v) => write!(f, "Lit<{:#x} : word64>", v),
+            Lit::Unit => write!(f, "Lit<()>"),
         }
     }
 }
@@ -88,13 +90,36 @@ impl fmt::Display for Constr {
     }
 }
 
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct Param(pub String, pub SmlType);
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct ParseParamErr;
+
+impl FromStr for Param {
+    type Err = ParseConstrErr;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match parse_param(s) {
+            Ok(("", param)) => Ok(param),
+            _ => Err(ParseConstrErr),
+        }
+    }
+}
+
+impl fmt::Display for Param {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Param<{} : {}>", self.0, self.1)
+    }
+}
+
 define_language! {
     pub enum FPeg {
         // A literal of an SML primitive type
         Literal(Lit),
 
         // Represents a block argument
-        Param(String),
+        Parameter(Param),
 
         // Represents a call of an SML primitive function
         CallPrim(Prim, Box<[Id]>),
@@ -113,10 +138,11 @@ mod tests {
 
     #[test]
     fn test_lit_to_str() {
-        assert_eq!(format!("{}", Lit::Word8(0)), "Word8(0)");
-        assert_eq!(format!("{}", Lit::Word32(0)), "Word32(0)");
-        assert_eq!(format!("{}", Lit::Word64(0)), "Word64(0)");
-        assert_eq!(format!("{}", Lit::Unit), "Unit()");
+        assert_eq!(format!("{}", Lit::Word8(0)), "Lit<0x0 : word8>");
+        assert_eq!(format!("{}", Lit::Word16(0)), "Lit<0x0 : word16>");
+        assert_eq!(format!("{}", Lit::Word32(0)), "Lit<0x0 : word32>");
+        assert_eq!(format!("{}", Lit::Word64(0)), "Lit<0x0 : word64>");
+        assert_eq!(format!("{}", Lit::Unit), "Lit<()>");
     }
 
     #[test]
@@ -138,5 +164,30 @@ mod tests {
     }
 
     #[test]
-    fn test_parse_fpeg() {}
+    fn test_fpeg() {
+        let w32 = "word32";
+        let mut e: RecExpr<FPeg> = RecExpr::default();
+        let x_e = e.add(FPeg::Parameter(Param("x".to_owned(), w32.to_owned())));
+        let y_e = e.add(FPeg::Parameter(Param("y".to_owned(), w32.to_owned())));
+        let add_e = e.add(FPeg::CallPrim(Prim("add".to_owned()), Box::new([x_e, y_e])));
+        let lit2_e = e.add(FPeg::Literal(Lit::Word32(2)));
+        let _ = e.add(FPeg::CallPrim(
+            Prim("mul".to_owned()),
+            Box::new([lit2_e, add_e]),
+        ));
+
+        assert_eq!(
+            e.to_string(),
+            "(Prim<mul> \"Lit<0x2 : word32>\" (Prim<add> \"Param<x : word32>\" \"Param<y : word32>\"))"
+        );
+
+        let mut egraph: EGraph<FPeg, ()> = EGraph::default();
+        let e_id = egraph.add_expr(&e);
+        egraph.rebuild();
+
+        assert_eq!(Some(e_id), egraph.lookup_expr(&e));
+
+        let e_parsed: RecExpr<FPeg> = "(Prim<mul> \"Lit<0x2 : word32>\" (Prim<add> \"Param<x : word32>\" \"Param<y : word32>\"))".parse().unwrap();
+        assert_eq!(Some(e_id), egraph.lookup_expr(&e_parsed));
+    }
 }
