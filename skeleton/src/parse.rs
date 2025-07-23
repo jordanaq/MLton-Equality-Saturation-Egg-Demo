e823caa2d1a9af8732536aa9acdbe078b643b0e8
jordanaq <quinn.jordan@protonmail.ch>
2025-07-23 03:22:00 -0400

    Started the parsing and further fleshed out the structs

--- skeleton/src/parse.rs
+++ skeleton/src/parse.rs
@@ -1,1 +1,59 @@
+use nom::{
+    bytes::complete::{tag, take_until},
+    character::complete::{
+        multispace0,
+        multispace1
+    },
+    sequence::{delimited, preceded},
+    IResult,
+    Parser
+};
 
+use crate::skeleton::{
+    Block,
+    FuncId,
+    Skeleton,
+    Transfer
+};
+
+use parse_utils::{
+    paren_list,
+    word,
+};
+
+// use e_graph::{ FPeg, Region };
+
+impl Skeleton {
+    fn parse_comment<'a>(&'a self, s: &'a str) -> IResult<&'a str, &'a str> {
+        delimited(tag("/*"), take_until("*/"), tag("*/")).parse(s)
+    }
+    
+    fn parse_transfer_goto<'a>(&'a mut self, fun_id: FuncId, s: &'a str) -> IResult<&'a str, Transfer> {
+        let (rest, (b, rs)) : (&str, (&str, Vec<&str>)) =
+            preceded(
+                (tag("goto"), multispace1),
+                (word(), preceded(multispace0, paren_list()))
+            ).parse(s)?;
+
+        let b_id = match self.find_block_by_name(b) {
+            Some(b) => b.id,
+            None => {
+                let b_id = self.fresh_id();
+                self.add_block(
+                    fun_id, 
+                    Block {
+                        id: b_id,
+                        name: b.to_string(),
+                        inputs: vec![],
+                        transfer: Transfer::Bug,
+                    }
+                ).unwrap();
+                b_id
+            }
+        };
+
+        let r_ids = rs.iter().map(|r_s| self.find_region_by_name(r_s).unwrap()).collect();
+        
+        Ok((rest, Transfer::Goto(b_id, r_ids)))
+    }
+}
--- skeleton/src/skeleton.rs
+++ skeleton/src/skeleton.rs
@@ -1,22 +1,26 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
+use std::collections::hash_map;
 
-use e_graph::{ FPeg, Region };
+use std::hash::Hash;
+
+use e_graph::fpeg::{ FPeg, Region };
 
 use sml_utils::SmlType;
 
-type ArgId = u64;
-type BlockId = u64;
-type GlobalId = u64;
-type FuncId = u64;
+type IdType = u64;
+pub(crate) type ArgId = IdType;
+pub(crate) type BlockId = IdType;
+pub(crate) type FuncId = IdType;
 
 #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
-struct BlockArg {
+pub(crate) struct BlockArg {
     id: ArgId,
     arg_t: SmlType,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
-enum Transfer {
+pub(crate) enum Transfer {
+    Bug,
     Goto(BlockId, Vec<Region>),
     Call,
     Raise,
@@ -25,15 +29,140 @@ enum Transfer {
 }
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
-struct Block {
-    id: BlockId,
-    inputs: Vec<BlockArg>,
-    transfer: Transfer,
+pub(crate) struct Block {
+    pub(crate) id: BlockId,
+    pub(crate) name: String,
+    pub(crate) inputs: Vec<BlockArg>,
+    pub(crate) transfer: Transfer,
 }
 
-#[derive(Debug, Clone, PartialEq, Eq)]
-pub struct Skeleton {
-    graph: FPeg,
-    root: Block,
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub(crate) struct FuncArg { 
+    id: ArgId,
+    arg_t: SmlType,
+}
+
+#[derive(Debug, Clone)]
+pub(crate) struct Func {
+    name: String,
+    inputs: Vec<FuncArg>,
+    return_t: SmlType,
+    entry: BlockId,
     blocks: HashMap<BlockId, Block>,
+}
+
+impl PartialEq for Func {
+    fn eq(&self, other: &Self) -> bool {
+        self.name == other.name
+            && self.inputs == other.inputs
+            && self.return_t == other.return_t
+    }
+}
+
+impl Eq for Func {}
+
+impl Hash for Func {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.name.hash(state);
+        self.inputs.hash(state);
+        self.return_t.hash(state);
+    }
+}
+
+impl Func {
+    fn add_block(&mut self, b: Block) -> Result<BlockId, String> {
+        let id = b.id;
+        match self.blocks.entry(id) {
+            hash_map::Entry::Occupied(o) =>
+                Err(format!("Attempt to create block with existing id `{}`", o.key())),
+            hash_map::Entry::Vacant(v) => {
+                v.insert(b);
+                Ok(id)
+            }
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub(crate) struct DatatypeConstructor {
+    name: String,
+    inputs: Vec<SmlType>
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub(crate) struct Datatype {
+    name: String,
+    constructors: Vec<DatatypeConstructor>
+}
+
+
+#[derive(Debug, Clone)]
+pub struct Skeleton {
+    curr_id_gen: u64,
+    funcs: HashMap<FuncId, Func>,
+    datatyeps: HashSet<Datatype>,
+    graph: FPeg,
+}
+
+impl Skeleton {
+    pub fn new() -> Self {
+        Skeleton {
+            curr_id_gen: 0,
+            funcs: HashMap::<FuncId, Func>::default(),
+            datatyeps: HashSet::<Datatype>::default(),
+            graph: FPeg::default()
+        }
+    }
+
+    pub(crate) fn fresh_id(&mut self) -> u64 {
+        self.curr_id_gen += 1;
+        self.curr_id_gen - 1
+    }
+
+    fn block_map(&self) -> HashMap<BlockId, &Block> {
+        self.funcs
+            .values()
+            .flat_map(|f| f.blocks.iter().map(|(&k, v)| (k, v)))
+            .collect::<HashMap<_, _>>()
+    }
+
+    fn blocks(&self) -> HashSet<&Block> {
+        self.funcs
+            .values()
+            .map(|f| f.blocks.values())
+            .flatten()
+            .collect::<HashSet<_>>()
+    }
+
+    pub(crate) fn find_block_by_name(&self, name: &str) -> Option<&Block> {
+        self.blocks()
+            .iter()
+            .map(|&b| b)
+            .find(|&b| b.name == name)
+    }
+
+    pub(crate) fn find_region_by_name(&self, name: &str) -> Option<Region> {
+        self.graph.find_region_by_name(name)
+    }
+
+    pub(crate) fn add_block(&mut self, func_id: FuncId, b: Block) -> Result<BlockId, String> {
+        let fun = self.funcs.get_mut(&func_id).expect(&format!("Invalid function id `{}", func_id));
+        fun.add_block(b)
+    }
+
+    fn valid(&self) -> bool {
+        // Only one block with each id
+        self.blocks().len() == self.block_map().len()
+        
+        // Each id matches in its map
+            && self.block_map()
+                .iter()
+                .all(|(&k, v)| k == v.id)
+    }
+}
+
+impl Default for Skeleton {
+    fn default() -> Self {
+        Self::new()
+    }
 }
